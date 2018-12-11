b6e4b3ad4790d6b0b8e7b1d1d008ad354bbecb42
MelTe <mtemimi@gmail.com>
2018-12-11 21:44:10 +0100

    Fixed basedir issue in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-BASEDR=$(basedir ${0})
+BASEDR=$(dirname ${0})
 source ${BASEDR}/project-happydeal-viur.env
 
 echo "Executing: " /usr/bin/docker build -t ${MY_CONTAINER_REGISTRY}/${MY_PROJECT_ID}/${MY_CONTAINER_NAME}:${MY_PROJECT_VERSION} .
