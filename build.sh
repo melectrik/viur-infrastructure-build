8d2ff74b2721623d52643e9a1ce0f911862a6471
MelTe <mtemimi@gmail.com>
2018-12-11 15:16:11 +0100

    Shaping environment file and producing some more echo for debug purposes

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash 
 
-export MY_PROJECT_ID="happydeal-viur"
-export MY_PROJECT_VERSION="v3.0.3"
-export MY_CONTAINER_NAME="deal-viur"
-export MY_CONTAINER_REGISTRY="eu.gcr.io"
-export MY_CLUSTER_ZONE="europe-west3-a"
-export MY_CLUSTER_NAME="deal"
-export MY_DEPLOYMENT_NAME="deal-deployment"
+source project-happydeal-viur.env
 
+echo "Executing: " /usr/bin/docker build -t ${MY_CONTAINER_REGISTRY}/${MY_PROJECT_ID}/${MY_CONTAINER_NAME}:${MY_PROJECT_VERSION} .
 /usr/bin/docker build -t ${MY_CONTAINER_REGISTRY}/${MY_PROJECT_ID}/${MY_CONTAINER_NAME}:${MY_PROJECT_VERSION} .
+
+echo "Executing: " /usr/bin/docker build -t ${MY_CONTAINER_REGISTRY}/${MY_PROJECT_ID}/${MY_CONTAINER_NAME}:latest .
+/usr/bin/docker build -t ${MY_CONTAINER_REGISTRY}/${MY_PROJECT_ID}/${MY_CONTAINER_NAME}:latest .
+
+echo "Executing: " /usr/bin/docker push ${MY_CONTAINER_REGISTRY}/${MY_PROJECT_ID}/${MY_CONTAINER_NAME}:${MY_PROJECT_VERSION}
 /usr/bin/docker push ${MY_CONTAINER_REGISTRY}/${MY_PROJECT_ID}/${MY_CONTAINER_NAME}:${MY_PROJECT_VERSION}
+
+echo "Executing: " /usr/bin/docker push ${MY_CONTAINER_REGISTRY}/${MY_PROJECT_ID}/${MY_CONTAINER_NAME}:latest
+/usr/bin/docker push ${MY_CONTAINER_REGISTRY}/${MY_PROJECT_ID}/${MY_CONTAINER_NAME}:latest
 
