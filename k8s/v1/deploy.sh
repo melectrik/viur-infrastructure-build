5b43447db28f1a4ee4b1afa8aa688b0e3749c37d
MelTe <mtemimi@gmail.com>
2018-12-11 22:08:43 +0100

    k8s deployment v1

---
+++ k8s/v1/deploy.sh
@@ -0,0 +1,18 @@
+BASEDR=$(dirname ${0})
+source ${BASEDR}/../../project-happydeal-viur.env
+
+gcloud config set compute/zone ${MY_CLUSTER_ZONE}
+
+gcloud container clusters create ${MY_CLUSTER_NAME} --num-nodes=${MY_CLUSTER_NODENUMBER_START}
+
+gcloud compute instances list
+
+kubectl run ${MY_DEPLOYMENT_NAME} --image=${MY_CONTAINER_REGISTRY}/${MY_PROJECT_ID}/${MY_CONTAINER_NAME} :${MY_PROJECT_VERSION} --port=${MY_PORT_INTERNAL}
+
+kubectl get pods
+
+kubectl expose deployment ${MY_DEPLOYMENT_NAME} --type=LoadBalancer --port 80 --target-port ${MY_PORT_INTERNAL}
+
+kubectl get service ${MY_DEPLOYMENT_NAME}
+
+
