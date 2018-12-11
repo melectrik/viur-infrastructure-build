e462242ef2eb62231337d360e93023e9e89b73f0
MelTe <mtemimi@gmail.com>
2018-12-11 22:42:33 +0100

    Added project scope to cluster clean up

--- k8s/v1/clean_up.sh
+++ k8s/v1/clean_up.sh
@@ -1,7 +1,7 @@
 BASEDR=$(dirname ${0})
 source ${BASEDR}/../../project-happydeal-viur.env
 
-kubectl delete service ${MY_DEPLOYMENT_NAME}
+kubectl delete service ${MY_DEPLOYMENT_NAME} --project=${MY_PROJECT_ID}
 
-gcloud container clusters delete ${MY_CLUSTER_NAME}
+gcloud container clusters delete ${MY_CLUSTER_NAME} --project=${MY_PROJECT_ID}
 
