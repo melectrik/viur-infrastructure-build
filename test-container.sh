c6207aca683b8543cbb103f4d8ebae261f0f3fa0
MelTe <mtemimi@gmail.com>
2018-12-11 21:49:24 +0100

    Fixed basedir issue in test-container.sh

--- test-container.sh
+++ test-container.sh
@@ -1,7 +1,5 @@
-!#/bin/bash 
-
-source project-happydeal-viur.env
-
+BASEDR=$(dirname ${0})
+source ${BASEDR}/project-happydeal-viur.env
 
 echo "Executing: " /usr/bin/docker run --rm -p ${MY_PORT_INTERNAL}:${MY_PORT_EXTERNAL} ${MY_CONTAINER_REGISTRY}/${MY_PROJECT_ID}/${MY_CONTAINER_NAME}:latest
 /usr/bin/docker run --rm -p ${MY_PORT_INTERNAL}:${MY_PORT_EXTERNAL} ${MY_CONTAINER_REGISTRY}/${MY_PROJECT_ID}/${MY_CONTAINER_NAME}:latest
